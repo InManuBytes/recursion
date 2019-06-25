80da2ab9e84a378f3afb78268af05802a5f5f549
Manuela Gomez <mmanu.gomez@gmail.com>
2019-06-24 21:17:43 -0400

    Includes cases for primitive types except undefined

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,27 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
+//converts a JavaScript object or value to a JSON string, 
+//optionally replacing values if a replacer function is specified or 
+//optionally including only the specified properties if a replacer array is specified
 
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let stringVersion = "";
+  if (typeof obj === 'number'){
+    return String(obj);
+  } else if (obj === null){
+    return 'null';
+  } else if (typeof obj === 'boolean'){
+    if (obj){
+      return 'true';
+    } else {
+      return 'false';
+    }
+  } else if (typeof obj === 'string'){
+    return "\""+obj+"\"";
+  } else if (Array.isArray(obj) === true){
+    return "[]";
+  }
 };
+
